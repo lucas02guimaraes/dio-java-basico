8e3f1c9c717717247d71cd40a918f2bafba85351
lucas02guimaraes <lucas02guimaraes@gmail.com>
2025-03-03 13:12:09 -0300

    Organização de Pacotes Java - Anatomia das Classes - Aula 5

--- java-anatomia-classes/src/edu/lucas/primeirasemana/BoletimEstudantil.java
+++ java-anatomia-classes/src/edu/lucas/primeirasemana/BoletimEstudantil.java
@@ -1,3 +1,5 @@
+package edu.lucas.primeirasemana;
+
 public class BoletimEstudantil {
     public static void main(String[] args) {
         int mediaFinal = 6;
--- java-anatomia-classes/src/edu/lucas/segundasemana/MinhaClasse.java
+++ java-anatomia-classes/src/edu/lucas/segundasemana/MinhaClasse.java
@@ -1,3 +1,5 @@
+package edu.lucas.segundasemana;
+
 public class MinhaClasse {
 
     public static void main(String[] args) {
