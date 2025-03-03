27c7c325119bc6a6ee1899fba42375d3205fb92e
lucas02guimaraes <lucas02guimaraes@gmail.com>
2025-03-03 12:12:24 -0300

    Comentarios de Variaveis validas e invalidas - Aula 1

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -3,6 +3,23 @@ public class MinhaClasse {
 public static void main (String [] args) {
     System.out.print ( "Olá turma, sejam bem-vindos");
 
+    /*
+     * Declaração inválida de variáveis
+     * 
+     * int numero&um = 1; //Os únicos símbolos permitidos são _ e $
+     * int 1numero = 1;     //Uma variável não pode começar com números
+     * int numero um = 1;   //Não pode ter espaço no nome da variável
+     * int long = 1;    //long faz parte das palavras reservadas da linguagem
+    */
+
+    /*
+     * Declaração válida de 
+     * 
+     * int numero$um = 1;
+     * int numero1 = 1;
+     * int numeroum = 1;
+     * int longo = 1;
+     */
 
 }
 
